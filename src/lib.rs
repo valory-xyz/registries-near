37008f46c8a36581e407976b559d4816ff964ade
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2024-09-16 17:16:58 +0100

    feat: nft mint

--- src/lib.rs
+++ src/lib.rs
@@ -79,16 +79,16 @@ impl ServiceRegistry {
         let tm = TokenMetadata {
             title: Some(String::default()), // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
             description: Some(String::default()), // free-form description
-            media: Some(String::default()), // URL to associated media, preferably to decentralized, content-addressed storage
-            media_hash: Some(Base64VecU8(vec![])), // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+            media: None, // URL to associated media, preferably to decentralized, content-addressed storage
+            media_hash: None, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
             copies: Some(1u64), // number of copies of this set of metadata in existence when token was minted.
-            issued_at: Some(String::default()), // ISO 8601 datetime when token was issued or minted
-            expires_at: Some(String::default()), // ISO 8601 datetime when token expires
-            starts_at: Some(String::default()), // ISO 8601 datetime when token starts being valid
-            updated_at: Some(String::default()), // ISO 8601 datetime when token was last updated
-            extra: Some(String::default()), // anything extra the NFT wants to store on-chain. Can be stringified JSON.
-            reference: Some(String::default()), // URL to an off-chain JSON file with more info.
-            reference_hash: Some(Base64VecU8(vec![])),
+            issued_at: None, // ISO 8601 datetime when token was issued or minted
+            expires_at: None, // ISO 8601 datetime when token expires
+            starts_at: None, // ISO 8601 datetime when token starts being valid
+            updated_at: None, // ISO 8601 datetime when token was last updated
+            extra: None, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
+            reference: None, // URL to an off-chain JSON file with more info.
+            reference_hash: None,
         };
         let token_id = "0".to_string();
         self.tokens.internal_mint(token_id.clone(), service_owner, Some(tm));
